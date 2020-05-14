d795e11e7d538dd705bdf9cfcc179838968284f0
btemovska <btemovska@gmail.com>
2020-05-13 20:23:07 -0400

    Final submission cookie orders

--- src/main/java/cookies/CookieOrder.java
+++ src/main/java/cookies/CookieOrder.java
@@ -1,4 +1,20 @@
 package cookies;
 
 public class CookieOrder {
+
+    private int numBoxes;
+    private String variety;
+
+    public CookieOrder(String localVariety, int localNumBoxes) { //constructions
+        this.numBoxes = localNumBoxes;
+        this.variety = localVariety;
+    }
+
+    public int getNumberBoxes() { //accessors
+        return this.numBoxes;
+    }
+
+    public String getVariety() { //accessors
+        return this.variety;
+    }
 }
